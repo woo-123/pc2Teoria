d4eac26748d07506e1dfeb7548e91efd2124613f
Diego Chiroque <diego-al-18@hotmail.com>
2021-09-14 18:45:42 -0500

    pc2Teoria

---
+++ Models/Registro.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace pc2Teoria.Models
+{
+     public class Registro
+    {
+        public string Nombre {get;set;}
+        public string ApellidoPaterno {get;set;}
+        public string ApellidoMaterno {get;set;}
+        public string TipoMascota {get;set;}
+        public string Descripcion {get;set;}
+        public int Telefono {get;set;}
+        public string Distrito {get;set;}
+        public string Direccion {get;set;}
+        public int Id {get;set;}
+    }
+}
